43dc4f0791759ed7d73d126b9766c8a12b28d991
DaNNeT9 <121464201+DaNNeT9@users.noreply.github.com>
2024-04-29 21:43:47 +0300

    Add files via upload

---
+++ log.js
@@ -0,0 +1,33 @@
+// let emailInput = document.querySelector('.emailInput');
+// let passwordInput = document.querySelector('.passwordInput');
+let homeNav = document.querySelector('.btn');
+
+// emailInput.addEventListener('input', toggleButton);
+// passwordInput.addEventListener('input', toggleButton);
+
+// function toggleButton() {
+//     const email = emailInput.value;
+//     const password = passwordInput.value;
+
+//     if (email.trim() !== '' && password.trim() !== '') {
+//         loginBtn.removeAttribute('disabled');
+//     } else {
+//         loginBtn.setAttribute('disabled', true);
+//     }
+// }
+
+homeNav.addEventListener('click', () => {
+    // const email = emailInput.value.trim();
+    // const password = passwordInput.value.trim();
+
+    // if (email !== '' && password !== '') {
+        toHomePage();
+    // }
+});
+
+function toHomePage() {
+    var homePage = "indexmain1.html";
+    window.location.href = homePage;
+};
+
+// disabled="True"
